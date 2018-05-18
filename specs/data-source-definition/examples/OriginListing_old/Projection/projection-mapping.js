ad7da7e14b6ce820d0330e3c77d03cff11952c45
Brandon Ramirez <brandon.ramirez@live.com>
2018-05-17 17:43:11 -0700

    Refactor mapping to be completely declarative using fluent API

---
+++ specs/data-source-definition/examples/OriginListing_old/Projection/projection-mapping.js
@@ -0,0 +1,32 @@
+const configureIPFSResolver = resolvers =>
+  resolvers.ipfs.files.json
+    // Using a string here guarantees that we control function execution, and that
+    // there are no closures.
+    .transformKey('key => "QM"+key')
+
+
+const listingProjector = (storageRoot, resolvers) =>
+  storageRoot
+    .get('listings')
+    .resolve('ipfsHash', configureIPFSResolver(resolvers))
+    .rename(['index', 'id'], ['ipfsHash', 'data'])
+    .selectFields(
+      'index',
+      'lister',
+      'price',
+      'unitsAvailable',
+      ['data', 'schema']
+      ['data', 'category'],
+      ['data', 'category'],
+      ['data', 'description'],
+    )
+    .flatten()
+
+export default {
+  ForSaleListing: listingProjector
+    .filter('schema','https://localhost:3000/schemas/for-sale.json')
+    .excludeFields('schema'),
+  AnnouncementListing: listingProjector
+    .filter('schema','https://localhost:3000/schemas/announcement.json')
+    .excludeFields('schema'),
+}
